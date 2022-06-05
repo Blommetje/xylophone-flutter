be384ee23e8c39d7eb89c9054c068b889cd0fb7e
Alexander Blommestijn <ablommestijn@vsh-tech.com>
2022-06-05 11:12:37 +0200

    First push

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 void playNote(int soundNumber) {
-   {
+  {
     final player = AudioCache();
     player.play('note$soundNumber.wav');
-  };
+  }
 }
 
 class XylophoneApp extends StatelessWidget {
@@ -17,85 +17,96 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
-          minimum: const EdgeInsets.all(32),
-            child: SizedBox(
-                width: 80,
-              child: Column(
-              children: [
-            TextButton(
-                    style: TextButton.styleFrom(
-                        backgroundColor: Colors.green ),
-                  onPressed:() {
-                    playNote(1);
-                  },
-              child: Text('Green',
-                style: TextStyle(fontSize: 22),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.green),
+                onPressed: () {
+                  playNote(1);
+                },
+                child: Text(
+                  'Green',
+                  style: TextStyle(fontSize: 22),
+                ),
               ),
             ),
-            TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.red ),
-              onPressed: () {
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.red),
+                onPressed: () {
                   playNote(2);
-                  },
-              child: Text('Red',
-                style: TextStyle(fontSize: 22),
+                },
+                child: Text(
+                  'Red',
+                  style: TextStyle(fontSize: 22),
+                ),
               ),
             ),
-            TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.blue ),
-              onPressed: () {
-                playNote(3);
-              },
-              child: Text('Blue',
-                style: TextStyle(fontSize: 22),
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.amber),
+                onPressed: () {
+                  playNote(3);
+                },
+                child: Text(
+                  'Blue',
+                  style: TextStyle(fontSize: 22),
+                ),
               ),
             ),
-            TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.pink ),
-              onPressed: () {
-                playNote(4);
-              },
-              child: Text('Pink',
-                style: TextStyle(fontSize: 22),
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.pink),
+                onPressed: () {
+                  playNote(4);
+                },
+                child: Text(
+                  'Pink',
+                  style: TextStyle(fontSize: 22),
+                ),
               ),
             ),
-
-            TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.deepOrangeAccent ),
-              onPressed: () {
-                playNote(5);
-              },
-              child: Text('Orange',
-                style: TextStyle(fontSize: 22),
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.deepOrangeAccent),
+                onPressed: () {
+                  playNote(5);
+                },
+                child: Text(
+                  'Orange',
+                  style: TextStyle(fontSize: 22),
+                ),
               ),
             ),
-            TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.yellow ),
-              onPressed: () {
-                playNote(6);
-              },
-              child: Text('Yellow',
-                style: TextStyle(fontSize: 22),
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.yellow),
+                onPressed: () {
+                  playNote(6);
+                },
+                child: Text(
+                  'Yellow',
+                  style: TextStyle(fontSize: 22),
+                ),
               ),
             ),
-            TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.purpleAccent ),
-              onPressed: () {
-                playNote(7);
-              },
-              child: Text('Purple',
-                style: TextStyle(fontSize: 22),
+            Expanded(
+              child: TextButton(
+                style:
+                    TextButton.styleFrom(backgroundColor: Colors.purpleAccent),
+                onPressed: () {
+                  playNote(7);
+                },
+                child: Text(
+                  'Purple',
+                  style: TextStyle(fontSize: 22),
+                ),
               ),
             ),
           ]),
         ),
-      ),
       ),
     );
   }
